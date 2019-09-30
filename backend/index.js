fccb605418b31b729d66bbf278044db45b4b66a4
Tiago Azevedo <tiagoemazevedo85@gmail.com>
2019-09-30 16:31:58 +0100

    B - knex migrations create tables

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const app = require('express')()
 const consign = require('consign')
 
+const db = require('./config/db')
+
+app.db = db
+
 consign()
     .then('./config/middlewares.js')
     .then('./api')
