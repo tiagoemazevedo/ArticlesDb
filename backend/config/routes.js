91ee2ce6e6697783fea6903568b01d9537dbb81d
Tiago Azevedo <tiagoemazevedo85@gmail.com>
2019-10-01 11:24:14 +0100

    B - categories API

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,8 +2,20 @@ module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
-    
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
+    
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+    //must come before more specific route (id)
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+    
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
